e95124f6dbb5375dd26e176df65ba473d6b5ba62
MaximeTTS <maxime.s.turquet@gmail.com>
2024-04-03 17:06:13 +0200

    Added a collapse content animation for the about page. Modified the code of the home and about pages, deleting some files. Implemented responsive phone format.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import React, { FunctionComponent } from 'react'
 import './_App.scss'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Header from './components/header/Header'
-import Footer from './components/footer/Footer'
 import Home from './pages/Home'
 import About from './pages/About'
 import Logement from './pages/Logement'
@@ -12,14 +10,12 @@ const App: FunctionComponent = () => {
   return (
     // Utilise BrowserRouter pour englober les Routes
     <Router>
-      <Header />
       <Routes>
         <Route path='/' element={<Home />} /> {/* Route pour la page d'accueil */}
         <Route path='/about' element={<About />} /> {/* Route pour la page À Propos */}
         <Route path='/logement' element={<Logement />} /> {/* Route pour la page Logement */}
         <Route path='*' element={<NotFoundPage />} /> {/* Route pour les chemins non trouvés */}
       </Routes>
-      <Footer />
     </Router>
   )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,22 +4,28 @@ import { Link } from 'react-router-dom'
 import Card from '../components/card/Card'
 import logements from '../data/logements.json'
 import slugify from '../helpers/slugify'
+import Footer from '../components/footer/Footer'
+import Header from '../components/header/Header'
 
 const Home: React.FC = () => {
   return (
-    <main>
-      <Banner />
-      <div className='gallery'>
-        {logements.map((logement) => {
-          const slug = slugify(logement.title)
-          return (
-            <Link to={`/logement/${slug}-${logement.id}`} key={logement.id} className='gallery__link'>
-              <Card title={logement.title} cover={logement.cover} />
-            </Link>
-          )
-        })}
-      </div>
-    </main>
+    <body>
+      <Header />
+      <main>
+        <Banner />
+        <div className='gallery'>
+          {logements.map((logement) => {
+            const slug = slugify(logement.title)
+            return (
+              <Link to={`/logement/${slug}-${logement.id}`} key={logement.id} className='gallery__link'>
+                <Card title={logement.title} cover={logement.cover} />
+              </Link>
+            )
+          })}
+        </div>
+      </main>
+      <Footer />
+    </body>
   )
 }
 
