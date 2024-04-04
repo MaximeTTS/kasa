de155cf413e9b029b3c1cdedd286fc3733577538
MaximeTTS <maxime.s.turquet@gmail.com>
2024-04-04 11:57:37 +0200

    Creation of the error page with its responsive.

--- src/components/errorPage/ErrorPage.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const ErrorPage: React.FC = () => {
-  return <div>Contenu de la page d'accueil</div>
-}
-
-export default ErrorPage
--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -1,7 +1,25 @@
+// Dans un fichier nommé ErrorPage.tsx
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Footer from '../components/footer/Footer'
+import Header from '../components/header/Header'
 
 const NotFoundPage: React.FC = () => {
-  return <div>Contenu de la page d'accueil</div>
+  return (
+    <body>
+      <Header />
+      <main>
+        <div className='error__div'>
+          <h1 className='error__h1'>404</h1>
+          <p className='error__message'>Oups! La page que vous demandez n'existe pas.</p>
+          <Link to='/' className='error__link'>
+            Retourner sur la page d’accueil
+          </Link>
+        </div>
+      </main>
+      <Footer />
+    </body>
+  )
 }
 
 export default NotFoundPage
